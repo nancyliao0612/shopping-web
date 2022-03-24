43f6808845ecdd7063a65e9202c7a3673d859a4e
Nancy <nancy62924@gmail.com>
2022-03-24 20:53:37 +0800

    add navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route
+          exact
+          path="/products/:productId"
+          children={<SingleProduct />}
+        ></Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
